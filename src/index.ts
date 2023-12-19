039f6480e743b6f20280b9a8e25065a082140c56
AnnAngela <naganjue@vip.qq.com>
2023-12-19 10:44:02 +0800

    feat: main feature

---
+++ src/index.ts
@@ -0,0 +1,62 @@
+"use strict";
+const isInGithubActions = process.env.GITHUB_ACTIONS === "true";
+import path from "path";
+import type { ESLint } from "eslint";
+import { debug, notice, warning, error } from "@actions/core";
+const deprecatedRules: string[] = [];
+const formatter: ESLint.Formatter["format"] = (results) => {
+    if (results.length === 0) {
+        return "Nothing is broken, everything is fine.";
+    }
+    for (const {
+        filePath, messages, usedDeprecatedRules,
+        // eslint-disable-next-line no-unused-vars
+        suppressedMessages, errorCount, fatalErrorCount, warningCount, fixableErrorCount, fixableWarningCount, output, source,
+    } of results) {
+        const baseAnnotationProperties = {
+            title: "ESLint Annotation",
+            file: filePath,
+        };
+        for (const { ruleId, replacedBy } of usedDeprecatedRules) {
+            if (deprecatedRules.includes(ruleId)) {
+                continue;
+            }
+            deprecatedRules.push(ruleId);
+            // @TODO: Switch to `warning` when eslint 9 is released, and use `baseAnnotationProperties` as second param
+            debug(`Deprecated rule: ${ruleId}. ${replacedBy.length > 0 ? `Please use ${replacedBy.join(" / ")} instead.` : ""} - https://eslint.org/docs/latest/rules/${ruleId}`);
+        }
+        for (const {
+            message, severity, line, column, endLine, endColumn, ruleId, fix,
+            // eslint-disable-next-line no-unused-vars
+            messageId, nodeType, fatal, source, suggestions,
+        } of messages) {
+            const msg = `${message} (${ruleId}) ${fix ? "[maybe fixable]" : ""} - https://eslint.org/docs/latest/rules/${ruleId}${isInGithubActions ? ` @ https://github.com/${process.env.GITHUB_REPOSITORY}/blob/${(process.env.GITHUB_SHAs as string).slice(0, 7)}/${path.relative(process.cwd(), filePath)}#L${line}${line !== endLine ? `-L${endLine}` : ""}` : ""}`;
+            /**
+             * @type {NonNullable<Parameters<notice>[1]>}
+             */
+            const annotationProperties = {
+                ...baseAnnotationProperties,
+                startLine: line,
+                endLine,
+                startColumn: column,
+                endColumn,
+            };
+            debug(JSON.stringify({ msg, ...annotationProperties }, null, 4));
+            switch (severity) {
+                case 0:
+                    notice(msg, annotationProperties);
+                    break;
+                case 1:
+                    warning(msg, annotationProperties);
+                    break;
+                case 2:
+                    error(msg, annotationProperties);
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+    return "";
+};
+module.exports = formatter;
