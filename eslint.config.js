0d7b2676128d5a4a9c25c853b913aa655d9b54ac
AnnAngela <naganjue@vip.qq.com>
2024-01-04 16:50:01 +0800

    chore: update eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,21 @@
-import { baseConfig, typescriptConfig } from "@annangela/eslint-config";
+import { baseConfig, nodeConfig, typescriptConfig } from "@annangela/eslint-config";
 /**
  * @type { import("eslint").Linter.FlatConfig[] }
  */
 const config = [
+    {
+        ignores: [
+            "dist"
+        ],
+    },
     { // Default config
         ...baseConfig,
+        ...nodeConfig,
+    },
+    {
+        rules: {
+            "n/no-sync": "off",
+        },
     },
     { // For TypeScript files in src/
         files: [
